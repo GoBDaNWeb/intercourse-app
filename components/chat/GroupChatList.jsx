b60b4f5ab93ec296bf98cbe33a7a67aff79054c7
GoBDaNWeb <bogdan.ognitsiy@yandex.ru>
2022-05-25 12:53:58 +0300

    intercourse 2.1.4

---
+++ components/chat/GroupChatList.jsx
@@ -0,0 +1,103 @@
+// * react/next
+import { useState, useEffect } from 'react';
+
+// * redux
+import {useSelector} from 'react-redux'
+
+// * supabase
+import { supabase } from 'utils/supabaseClient';
+import { fetchAllGroupChats } from 'utils/Store';
+
+// * components
+import PreviewGroupChat from './PreviewGroupChat';
+
+export default function GroupChatList() {
+    const [filteredChats, setFilteredChats] = useState(null)
+    const [searchChats, setSearchChats] = useState(null)
+
+    const {user} = useSelector(state => state.auth)
+    const {searchValue} = useSelector(state => state.chat)
+
+    const useStore = (props) => {
+        const [groupChats, setGroupChats] = useState([])
+        const [newGroupChat, handleNewGroupChat] = useState(null)
+    
+        useEffect(() => {
+            fetchAllGroupChats(setGroupChats)
+    
+            const groupChatListener = supabase
+                .from('group_chats')
+                .on('INSERT', payload => {
+                    handleNewGroupChat(payload.new)
+                })
+                .on('DELETE', payload => {handleNewGroupChat(payload.old)})
+                .subscribe()
+    
+            return () => {
+                groupChatListener.unsubscribe()
+            }
+        }, [])
+        
+        useEffect(() => {
+            if (newGroupChat) setGroupChats(groupChats.concat(newGroupChat))
+        }, [newGroupChat])
+    
+        return {groupChats}
+    }
+
+
+    // ** следим за персональными чатами и рендерим их 
+    const {groupChats} = useStore({})
+
+    // ** при изменении privatChats фильтруем чаты и записываем их в стейт filteredChats
+    useEffect(() => {
+        if (user !== null) {
+            const filteredByMembers = groupChats.filter(chat => {
+                const boolUsers = chat.members.map(item => item.id === user.id)
+                const containsTheCurrentUser = boolUsers.indexOf(true) != -1
+                chat.filtered_by_member = containsTheCurrentUser
+                return chat.filtered_by_member === true
+            })      
+            const filteredByCreated = groupChats.filter(chat => {
+                return user.id === chat.created_by.id
+            })
+            let tmp = []
+            filteredByMembers.forEach(item => {
+                tmp.push(item)
+                setFilteredChats([...tmp])
+            })
+            filteredByCreated.forEach(item => {
+                tmp.push(item)
+                setFilteredChats([...tmp])
+            })
+        } 
+    }, [groupChats])
+
+    // ** при изменении searchValue фильтруем чаты и записываем их в стейт searchChats
+    useEffect(() => {
+        if(filteredChats !== null) {
+            const search = filteredChats.filter(chat => chat.chat_title.toLowerCase().includes(searchValue.toLowerCase()))
+            setSearchChats(search)
+        }
+    }, [searchValue])
+
+    
+    return (
+        <div className='flex flex-col h-full overflow-y-auto w-full custom-scroll'>
+                {
+                    !searchValue.length && filteredChats !== null
+                    && filteredChats.map((chat) => (
+                        <div key={chat.id}>
+                            <PreviewGroupChat chatData={chat}/>
+                        </div>
+                    ))
+                }
+                {
+                    searchValue.length > 0 && searchChats !== null
+                    && searchChats.map((chat) => (
+                        <PreviewGroupChat key={chat.id} chatData={chat}/>
+                    ))
+                }
+        </div>
+    )
+}
---
+++ components/chat/PrivatChatList.jsx
@@ -0,0 +1,85 @@
+// * react/next
+import { useState, useEffect } from 'react';
+
+// * redux
+import {useSelector} from 'react-redux'
+import { supabase } from 'utils/supabaseClient';
+
+// * supabase
+import { fetchAllPrivatChats } from 'utils/Store';
+
+// * components
+import PreviewPrivatChat from 'components/chat/PreviewPrivatChat';
+
+export default function ChatList() {
+    const [filteredChats, setFilteredChats] = useState(null)
+    const [searchChats, setSearchChats] = useState(null)
+
+    const {searchValue} = useSelector(state => state.chat)
+    const {user} = useSelector(state => state.auth)
+
+    // ** при монтировании/размонтировании подписываемся/отписываемся на realtime
+    const useStore = (props) => {
+        const [privatChats, setPrivatChats] = useState([])
+        const [newPrivatChat, handleNewPrivatChat] = useState(null)
+    
+        useEffect(() => {
+            fetchAllPrivatChats(setPrivatChats)
+
+            const privatChatListener = supabase
+                .from('privat_chats')
+                .on('INSERT', payload => {
+                    handleNewPrivatChat(payload.new)
+                })
+                .on('DELETE', payload => {handleNewPrivatChat(payload.old)})
+                .subscribe()
+    
+    
+            return () => {
+                privatChatListener.unsubscribe()
+            }
+        }, [])
+    
+        useEffect(() => {
+            if (newPrivatChat) setPrivatChats(privatChats.concat(newPrivatChat))
+        }, [newPrivatChat])
+    
+        return {privatChats}
+    }
+
+    const {privatChats} = useStore({})
+
+    // ** при изменении privatChats фильтруем чаты и записываем их в стейт filteredChats
+    useEffect(() => {
+        const filtered = privatChats.filter(chat => user.id === chat.created_by.id || user.id === chat.interlocutor.id)
+        setFilteredChats(filtered)
+    }, [privatChats])
+
+    // ** при изменении searchValue фильтруем чаты и записываем их в стейт searchChats
+    useEffect(() => {
+        if(filteredChats !== null) {
+            const search = filteredChats.filter(chat => chat.chat_title.toLowerCase().includes(searchValue.toLowerCase()))
+            setSearchChats(search)
+        }
+    }, [searchValue])
+
+
+    return (
+            <div className='flex flex-col h-full overflow-y-auto w-full custom-scroll'>
+                {
+
+                    !searchValue.length && filteredChats !== null
+                    && filteredChats.map((chat) => (
+                        <PreviewPrivatChat key={chat.id} chatData={chat}/>
+                    ))
+                }
+                {
+                    searchValue.length > 0 && searchChats !== null
+                    && searchChats.map((chat) => (
+                        <PreviewPrivatChat key={chat.id} chatData={chat}/>
+                    ))
+                }
+            </div>
+        
+    )
+}
