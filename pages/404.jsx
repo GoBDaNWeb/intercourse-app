d7b0b4765f24c654085847b4c9e68df1fed57ff1
GoBDaNWeb <bogdan.ognitsiy@yandex.ru>
2022-05-28 23:23:58 +0300

    intercourse 2.2.6

--- pages/404.jsx
+++ pages/404.jsx
@@ -1,26 +1,10 @@
-// * react/next
-import {useRouter} from 'next/router'
-
-// * icons
-import {BsArrowLeftShort} from 'react-icons/bs'
-
 export default function Custom404() {
-    const router = useRouter()
-
     return (
         <div className='w-full h-full bg-primary flex items-center justify-center'>
-            <div className='flex flex-col items-center gap-5'>
-                <h3 className='text-primary text-center text-3xl font-semibold'>
-                    ooops.... <br/>
-                    it looks like you went to the wrong place
-                </h3>
-                <button
-                    onClick={() => router.back()}
-                    className='flex items-center gap-1 text-primary text-lg'
-                >
-                   <BsArrowLeftShort/> go back
-                </button>
-            </div>
+            <h3 className='text-primary text-center text-3xl font-semibold'>
+                ooops.... <br/>
+                it looks like you went to the wrong place
+            </h3>
         </div>
     )
 }
